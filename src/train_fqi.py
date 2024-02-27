a0b11680a409863a0bfce028f259cb4576e6a5b3
polossbn <paul.bonin@mines-paristech.fr>
2024-02-27 17:03:56 +0100

    new fqi

--- src/train_fqi.py
+++ src/train_fqi.py
@@ -1,9 +1,12 @@
 from gymnasium.wrappers import TimeLimit
+from evaluate import evaluate_HIV, evaluate_HIV_population
 from env_hiv import HIVPatient
 import numpy as np
 import pickle
+import random
+import os
 from tqdm import tqdm
-from sklearn.ensemble import RandomForestRegressor
+from sklearn.ensemble import ExtraTreesRegressor
 
 
 env = TimeLimit(
@@ -21,12 +24,86 @@ env = TimeLimit(
 ############################################################
 
 
+def seed_everything(seed: int = 42):
+    random.seed(seed)
+    os.environ["PYTHONHASHSEED"] = str(seed)
+    np.random.seed(seed)
+
+
 class ProjectAgent:
     def __init__(self):
-        self.batch_size = 2000
-        self.training_iterations = 100
-        self.gamma = 0.99
-        self.max_episode = 50
+        ### PARAMS
+        self.collect_size = 600
+        self.Q_iterations = 30
+        self.nb_epochs = 50
+        self.gamma = 0.98
+        ### END PARAMS
+        self.S = []
+        self.A = []
+        self.R = []
+        self.S2 = []
+        self.D = []
+        self.Q = None
+
+    def collect_samples(self, env, randomness=0.0):
+        s, _ = env.reset()
+        for _ in tqdm(range(self.collect_size)):
+            if np.random.rand() < randomness:
+                a = np.random.choice(4)
+            else:
+                a = self.greedy_action(s)
+            s2, r, done, trunc, _ = env.step(a)
+            self.S.append(s)
+            self.A.append(a)
+            self.R.append(r)
+            self.S2.append(s2)
+            self.D.append(done)
+            if done or trunc:
+                s, _ = env.reset()
+            else:
+                s = s2
+
+    def rf_fqi(self):
+        S = np.array(self.S)
+        A = np.array(self.A).reshape((-1, 1))
+        R = np.array(self.R)
+        S2 = np.array(self.S2)
+        D = np.array(self.D)
+        SA = np.append(S, A, axis=1)
+        Q = self.Q
+        for iter in tqdm(range(self.Q_iterations)):
+            if iter == 0 and Qfunction == None:
+                value = R.copy()
+            else:
+                Q2 = np.zeros((self.batch_size, 4))
+                for a2 in range(4):
+                    A2 = a2 * np.ones((S.shape[0], 1))
+                    S2A2 = np.append(S2, A2, axis=1)
+                    Q2[:, a2] = Qfunction.predict(S2A2)
+                max_Q2 = np.max(Q2, axis=1)
+                value = R + self.gamma * (1 - D) * max_Q2
+            Q = ExtraTreesRegressor(n_estimators=50, n_jobs=-1)
+            Q.fit(SA, value)
+            Qfunction = Q
+        self.model = Q
+
+    def greedy_action(self, s):
+        Qsa = []
+        for a in range(4):
+            sa = np.append(s, a).reshape(1, -1)
+            Qsa.append(self.model.predict(sa))
+        return np.argmax(Qsa)
+    
+    def train(self):
+        self.collect_samples(env, randomness=0.0)
+        self.rf_fqi()
+        print(0, evaluate_HIV(agent=self, nb_episode=5) / 10e6)
+        self.save("rf_model.pkl")
+        for epoch in range(self.nb_epochs):
+            self.collect_samples(env, randomness=0.1)
+            self.rf_fqi()
+            print(epoch + 1, evaluate_HIV(agent=self, nb_episode=5) / 10e6)
+            self.save("rf_model.pkl")
 
     def act(self, observation, use_random=False):
         return np.random.choice(4) if use_random else self.greedy_action(observation)
@@ -38,84 +115,6 @@ class ProjectAgent:
     def load(self):
         with open("rf_model.pkl", "rb") as f:
             self.model = pickle.load(f)
-
-    def collect_samples(self, env):
-        s, _ = env.reset()
-        S = []
-        A = []
-        R = []
-        S2 = []
-        D = []
-        for _ in tqdm(range(self.batch_size)):
-            a = env.action_space.sample()
-            s2, r, done, trunc, _ = env.step(a)
-            S.append(s)
-            A.append(a)
-            R.append(r)
-            S2.append(s2)
-            D.append(done)
-            if done or trunc:
-                s, _ = env.reset()
-            else:
-                s = s2
-        S = np.array(S)
-        A = np.array(A).reshape((-1, 1))
-        R = np.array(R)
-        S2 = np.array(S2)
-        D = np.array(D)
-        return S, A, R, S2, D
-
-    def train(self):
-        Qfunction = None
-        episode = 0
-        episode_cum_reward = 0
-        state, _ = env.reset()
-        S, A, R, S2, D = self.collect_samples(env)
-        SA = np.append(S, A, axis=1)
-        for episode in range(self.max_episode):
-            for iter in tqdm(range(self.training_iterations)):
-                if iter == 0:
-                    value = R.copy()
-                else:
-                    Q2 = np.zeros((self.batch_size, 4))
-                    for a2 in range(4):
-                        A2 = a2 * np.ones((S.shape[0], 1))
-                        S2A2 = np.append(S2, A2, axis=1)
-                        Q2[:, a2] = Qfunction.predict(S2A2)
-                    max_Q2 = np.max(Q2, axis=1)
-                    value = R + self.gamma * (1 - D) * max_Q2
-                Q = RandomForestRegressor()
-                Q.fit(SA, value)
-                Qfunction = Q
-            self.model = Q
-            while True:
-                action = self.act(state)
-                next_state, reward, done, trunc, _ = env.step(action)
-                episode_cum_reward += reward
-                if done or trunc:
-                    print(
-                        "Episode ",
-                        "{:3d}".format(episode),
-                        ", episode return ",
-                        "{:4.1f}".format(episode_cum_reward/10e6),
-                        sep="",
-                    )
-                    episode_cum_reward = 0
-                    state, _ = env.reset()
-                    S, A, R, S2, D = self.collect_samples(env)
-                    SA = np.append(S, A, axis=1)
-                    break
-                else:
-                    state = next_state
-            self.save("rf_model.pkl")
-
-    def greedy_action(self, s):
-        Qsa = []
-        for a in range(4):
-            sa = np.append(s, a).reshape(1, -1)
-            Qsa.append(self.model.predict(sa))
-        return np.argmax(Qsa)
-
 
 ############################################################
 ############################################################
